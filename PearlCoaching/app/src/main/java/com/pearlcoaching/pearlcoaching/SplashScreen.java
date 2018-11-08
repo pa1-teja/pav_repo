fb2259579e5d28ea1d194a83e8a740ae319a4d31
paletva1 <vamshee_krishna.paleti@novartis.com>
2018-11-08 23:53:41 +0530

    added handler in the splash screen.

--- PearlCoaching/app/src/main/java/com/pearlcoaching/pearlcoaching/SplashScreen.java
+++ PearlCoaching/app/src/main/java/com/pearlcoaching/pearlcoaching/SplashScreen.java
@@ -2,28 +2,29 @@ package com.pearlcoaching.pearlcoaching;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                goToNextScreen();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 
-        startActivity(new Intent(SplashScreen.this, LoginScreen.class));
-
-//        new CountDownTimer(3000, 1000) {
-//
-//            public void onTick(long millisUntilFinished) {
-//            }
-//
-//            public void onFinish() {
-//                startActivity(new Intent(SplashScreen.this, LoginScreen.class));
-//            }
-//        }.start();
-
-
+    private void goToNextScreen() {
+        startActivity(new Intent(SplashScreen.this, ServicesScreen.class));
     }
 }
